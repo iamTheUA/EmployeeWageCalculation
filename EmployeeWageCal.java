5b8d4f490234ad193d1fe7076d4c606280709b95
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-26 14:14:25 +0530

    Checking Present or not

---
+++ EmployeeWageCal.java
@@ -0,0 +1,19 @@
+package employee;
+import java.util.Random;
+
+public class EmployeeWageCal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		Random ran = new Random();
+		int isPresent= ran.nextInt(2);
+		if(isPresent==1) {
+			System.out.println("Present");
+		}
+		else
+			System.out.println("Absent");
+	}
+
+}
