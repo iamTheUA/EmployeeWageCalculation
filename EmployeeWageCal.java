880cbd387034478694009aac5a1e2d2d617f092c
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-26 15:52:08 +0530

    Monthly Wage

--- EmployeeWageCal.java
+++ EmployeeWageCal.java
@@ -7,9 +7,21 @@ public class EmployeeWageCal {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
+		int Hour=0;
+		int Wage=20;
+		int MonthlyWage=0;
+		for(int i=0; i<20; i++) {
+			calHour();
+			int DailyWage=Hour*Wage;
+			MonthlyWage=MonthlyWage+DailyWage;
+		}
+	
+	}
+	
+	static void calHour() {
 		Random ran = new Random();
 		int isPresent= ran.nextInt(3);
-		int Hour=0;		
+		int Hour=0;
 		switch (isPresent) {
 	    case 0:
 	        Hour=0;
@@ -22,10 +34,5 @@ public class EmployeeWageCal {
 	        Hour=8;
 	        break;
 		}
-			
-		
-		int Wage=20;
-		int DailyWage=Hour*Wage;
 	}
-
 }
